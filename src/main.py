99967cb730a8a71e31f5c9dfe7888c242a5f1282
haraldnilsen <harald_998@hotmail.com>
2023-10-16 14:11:09 +0200

    :construction: added env variable and selenium driver
    Co-authored-by: Sindre Kjelsrud <kjelsrudsindre@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-
+from scraping import get_available_mountains, get_mountain_information
+from selenium import webdriver
+import os
 
 # Functions
 def find_mountain():
+    browser = webdriver.Chrome(os.environ.get('DRIVER_LOCATION'))
+    options = webdriver.ChromeOptions()
+    options.add_argument("headless")
+
     user_mountain = input('Hvilket fjell skal du på? ')
     
+    mountain_name = get_available_mountains(browser, user_mountain)
+
+    get_mountain_information(browser, mountain_name)
+
 
 # Main Execution
 if __name__ == "__main__":
--- src/scraping.py
+++ src/scraping.py
@@ -6,22 +6,22 @@ from time import sleep
 from bs4 import BeautifulSoup
 import requests
 
-def get_available_mountains(browser:webdriver.Chrome, mountain_name:str):
+def get_available_mountains(browser:webdriver.Chrome, user_mountain:str) -> str:
     """Searches for available mountains based on the given mountain name and
     displays them. Takes user input to further refine search results.
 
     @param browser: The Selenium WebDriver object used for web navigation.
     @type browser: selenium.webdriver.Chrome
-    @param mountain_name: The name of the mountain to search for.
-    @type mountain_name: str
+    @param user_mountain: The name of the mountain to search for.
+    @type user_mountain: str
         
     @rtype: None
     """
     browser.get('https://ut.no/')
     sleep(0.3)
-    søk=browser.find_element_by_id('input-field')
-    søk.send_keys(mountain_name)
-    søk.send_keys(Keys.ENTER)
+    search=browser.find_element_by_id('input-field')
+    search.send_keys(user_mountain)
+    search.send_keys(Keys.ENTER)
     sleep(0.5)
     
     cookie=browser.find_element_by_class_name('c-button')
@@ -49,3 +49,31 @@ def get_available_mountains(browser:webdriver.Chrome, mountain_name:str):
             if teller >=10:
                       break
         bruker=int(input('Du søkte "{}", hvilken tur tenker du på? \n'.format(fjellvalg)))
+
+    valg=valg[(bruker)-1]
+    valg.click()
+    resultat=browser.current_url
+
+    return resultat
+
+def get_mountain_information(browser: webdriver.Chrome, mountain_name:str):
+    """Searches for available mountains based on the given mountain name and
+    displays them. Takes user input to further refine search results.
+
+    @param browser: The Selenium WebDriver object used for web navigation.
+    @type browser: selenium.webdriver.Chrome
+    @param website_url: The url of the webpage containing information about the mountain.
+    @type website_url: str
+    @param mountain_name: The name of the mountain to search for.
+    @type mountain_name: str
+        
+    @rtype: None
+    """
+
+    turinfo = browser.find_element_by_class_name('info-list')
+    turinfo = turinfo.text.split('\n')
+    teller=0
+    print('Her er informasjon om turen til {}:'.format(mountain_name),'\n')
+    for i in range(6):
+        print(turinfo[teller],'-',turinfo[teller+1])
+        teller=teller+2
